7aa866d20679c64e896650c84eea9b44f82a22eb
divergos <j.vergosr@hotmail.com>
2017-10-20 16:32:06 +0300

    AddOwnerForm->add id variable SearchController SearchFOrm->add afm and mail edit.ftl edit.js UserConverters User-> getter and setter for id

---
+++ src/main/java/com/codingschool/repairWeb/Converters/UserConverter.java
@@ -0,0 +1,22 @@
+package com.codingschool.repairWeb.Converters;
+
+import com.codingschool.repairWeb.Domain.User;
+import com.codingschool.repairWeb.Model.AddOwnerForm;
+
+public class UserConverter {
+
+    public static User buildEditUserObject(AddOwnerForm addForm  ) {
+        User user = new User();
+        user.setName(addForm.getName());
+        user.setMail(addForm.getMail());
+        user.setAddress(addForm.getAddress());
+        user.setAfm(addForm.getAfm());
+        user.setPass(addForm.getPass());
+        user.setSurname(addForm.getSurname());
+        user.setId(addForm.getId());
+        user.setVehicles(null);
+
+        return user;
+    }
+}
+
--- src/main/java/com/codingschool/repairWeb/Domain/User.java
+++ src/main/java/com/codingschool/repairWeb/Domain/User.java
@@ -53,6 +53,14 @@ public class User implements Serializable {
 
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public void addVeh(Vehicle veh){
         vehicles.add(veh);
     }
