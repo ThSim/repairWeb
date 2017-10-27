c9800dad4780b2f1e4a14cd307933ae363955057
ThSim <thesimd91@gmail.com>
2017-10-27 04:40:04 +0300

    Fixed footer position

--- src/main/java/com/codingschool/repairWeb/Converters/DateParser.java
+++ src/main/java/com/codingschool/repairWeb/Converters/DateParser.java
@@ -2,11 +2,14 @@ package com.codingschool.repairWeb.Converters;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+import static java.util.Locale.US;
 
 public class DateParser {
 
     public static LocalDateTime dateFromPicker(String date){
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy h:mm a");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy h:mm a", Locale.US);
         LocalDateTime result = LocalDateTime.parse(date, formatter);
         return result;
     }
